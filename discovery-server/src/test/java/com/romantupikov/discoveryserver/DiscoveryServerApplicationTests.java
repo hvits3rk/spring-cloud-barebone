3597a7583721d6188d0aa7e98300b688a7ba6473
Roman Tupikov <romkacg@gmail.com>
2018-08-15 09:10:22 +0300

    Eureka discovery server module added

--- discovery-server/src/test/java/com/romantupikov/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/romantupikov/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.romantupikov.configserver;
+package com.romantupikov.discoveryserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigserverApplicationTests {
+public class DiscoveryServerApplicationTests {
 
     @Test
     public void contextLoads() {
